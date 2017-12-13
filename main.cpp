43a1829545fd1b0e8c610fe14c9c631407396530
Marek Skalník <xskaln03@vutbr.cz>
2017-12-13 16:16:56 +0100

    c++ file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 2;
+    int y = 42;
+    cin >> y;
+    cout << "Hello world! " << x * y << " Ha!" << endl;
+    return 0;
+}
